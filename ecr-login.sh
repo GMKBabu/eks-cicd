4bb9c64da066abf4b8e9e610459dc7b852050f6f
GMKBabu <babu.m@connectio.co.in>
2020-04-03 17:47:19 +0530

    testing

--- ecr-login.sh
+++ ecr-login.sh
@@ -16,8 +16,8 @@ TOKEN=`aws ecr --region=$REGION get-authorization-token --output text --query au
 # Create or replace registry secret
 #
 
-kubectl delete secret --ignore-not-found $SECRET_NAME
-kubectl create secret docker-registry $SECRET_NAME \
+/root/bin/kubectl delete secret --ignore-not-found $SECRET_NAME
+/root/bin/kubectl create secret docker-registry $SECRET_NAME \
  --docker-server=https://${ACCOUNT}.dkr.ecr.${REGION}.amazonaws.com \
  --docker-username=AWS \
  --docker-password="${TOKEN}" \
